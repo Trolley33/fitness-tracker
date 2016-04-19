de2cfbb9d5156f6c326c7886fcaf77b95751df2d
Reece Trolley <reecetrolley@gmail.com>
2016-04-19 22:21:04 +0100

    * Did a lot of database work * Client can now request a feed and profiles * In the middle of adding ability to post * Need to add pages * Need to add sign up

--- gui-client.py
+++ gui-client.py
@@ -2,13 +2,86 @@ import select
 import queue
 import socket
 import random
+import threading
 import hashlib
 import string
 import tkinter as tk
+from tkinter import messagebox
 
 # the master server's IP, set as programmer
 global SERVER
 SERVER = ("Trolley", 54321)
+
+
+class Post:
+    def __init__(self, container, username, activity, date, text, user_id):
+        self.container = container
+        self.page_f = container.page_frame
+
+        self.frame = tk.Frame(self.page_f, bg="white", width=20, bd=2, relief="groove", padx=4)
+        self.frame.columnconfigure(0, minsize=150)
+        self.frame.columnconfigure(1, minsize=200)
+
+        self.username = username
+        self.activity = activity
+        self.date = date
+        self.text = text
+        self.user_id = user_id
+
+        self.user_lab = tk.Label(self.frame, text=self.username, fg="royalblue2", bg="white",
+                                 font=("trebuchet ms", 12, "bold"))
+        self.activity_lab = tk.Label(self.frame, text=self.activity, fg="black", bg="white",
+                                     font=("trebuchet ms", 12, "bold"))
+        self.profile_button = tk.Button(self.frame, text="View Profile", bd=0, fg="royalblue3", bg="white",
+                                        font=("trebuchet ms", 12, "bold"),
+                                        command=lambda: self.container.load(self.user_id))
+        self.date_lab = tk.Label(self.frame, text=self.date, fg="black", bg="white",
+                                 font=("trebuchet ms", 10), width=10)
+        self.text_lab = tk.Label(self.frame, text=self.text, fg="black", bg="white", wrap=480,
+                                 font=("trebuchet ms", 10), justify="left")
+
+    def draw(self, row):
+        self.frame.grid(column=0, row=row, pady=(5, 5), sticky="WE")
+
+        self.user_lab.grid(column=0, row=0, sticky="W", padx=5)
+        self.activity_lab.grid(column=1, row=0, sticky="W", padx=5)
+        if self.user_id != -1:
+            self.profile_button.grid(column=4, row=0, sticky="E", padx=(40, 0))
+
+        self.text_lab.grid(column=0, row=1, columnspan=5, sticky="W", padx=(10, 10))
+        self.date_lab.grid(column=4, row=2, sticky="E")
+
+    def delete(self):
+        self.frame.destroy()
+
+        self.user_lab.destroy()
+        self.activity_lab.destroy()
+        self.profile_button.destroy()
+        self.text_lab.destroy()
+        self.date_lab.destroy()
+        del self
+
+
+class PostDialog:
+    def __init__(self, container):
+        self.container = container
+
+        self.top = tk.Toplevel(self.container.app.root)
+
+        self.label = tk.Label(self.top, text="Write something:", font=("trebuchet ms", 12, "bold"))
+        self.text = tk.Text(self.top)
+        self.activity = tk.Listbox(self.top)
+        self.submit_but = tk.Button(self.top, text="Submit", command=self.submit)
+
+        self.draw()
+
+    def draw(self):
+        self.label.grid(column=0, row=0)
+        self.text.grid(column=1, row=0)
+        self.submit_but.grid(column=1, row=1)
+
+    def submit(self):
+        print(self.text.get('1.0', 'end'))
 
 
 class App:
@@ -16,6 +89,10 @@ class App:
         self.root = tk.Tk()
         self.root.title("FitBook")
         self.root.configure(bg="royalblue2")
+        self.root.resizable(width=False, height=False)
+
+        self.id = -1
+        self.username = ""
 
         self.s = socket.socket()
 
@@ -25,8 +102,14 @@ class App:
         self.in_queue = queue.Queue()
 
         self.login_screen = Login(self)
+        self.main = Main(self)
 
         self.login_screen.draw()
+
+        t = threading.Thread(target=self.handler, args=(self.s, SERVER))
+        t.setDaemon(True)
+        t.start()
+
         self.root.mainloop()
 
     def handler(self, s, a):
@@ -39,14 +122,25 @@ class App:
                     if not reply:
                         break
                     print('received', reply)
+                    self.in_queue.put(reply)
                 if not self.out_queue.empty():
                     msg = self.out_queue.get()
                     s.send(msg.encode())
+                    print("sent:", msg)
             except Exception as e:
                 print(e)
                 break
         s.close()
         print("closed connection with", a)
+
+    @staticmethod
+    def popup(box, msg):
+        if box == "info":
+            messagebox.showinfo("Information", msg)
+        if box == "warning":
+            messagebox.showwarning("Warning", msg)
+        if box == "error":
+            messagebox.showerror("Error", msg)
 
 
 class Login:
@@ -59,11 +153,11 @@ class Login:
 
         self.mini_frame = tk.Frame(self.main_frame, bg="royalblue2")
 
-        self.user_lab = tk.Label(self.mini_frame, text="Username", font=("trebuchet ms", 13), bg="royalblue2",
+        self.user_lab = tk.Label(self.mini_frame, text="Username:", font=("trebuchet ms", 13), bg="royalblue2",
                                  fg="white")
         self.user_entry = tk.Entry(self.mini_frame, font=("trebuchet ms", 10))
 
-        self.pass_lab = tk.Label(self.mini_frame, text="Password", font=("trebuchet ms", 13), bg="royalblue2",
+        self.pass_lab = tk.Label(self.mini_frame, text="Password:", font=("trebuchet ms", 13), bg="royalblue2",
                                  fg="white")
         self.pass_entry = tk.Entry(self.mini_frame, font=("trebuchet ms", 10), show="•")
 
@@ -73,6 +167,8 @@ class Login:
                                       command=self.login, font=("trebuchet ms", 12))
 
     def draw(self):
+        self.app.root.configure(bg="royalblue2")
+
         self.title.grid(column=0, row=0, sticky="NEWS")
 
         self.main_frame.grid(column=0, row=1, padx=20, pady=(4, 8))
@@ -87,25 +183,161 @@ class Login:
         self.signup_button.grid(column=0, row=2, sticky="E", padx=5, pady=(5, 0))
         self.login_button.grid(column=1, row=2, sticky="W", padx=5, pady=(5, 0))
 
+    def undraw(self):
+        self.title.grid_forget()
+
+        self.main_frame.grid_forget()
+
+        self.mini_frame.grid_forget()
+        # in frame
+        self.user_lab.grid_forget()
+        self.user_entry.grid_forget()
+        self.pass_lab.grid_forget()
+        self.pass_entry.grid_forget()
+
+        self.signup_button.grid_forget()
+        self.login_button.grid_forget()
+
+        self.user_entry.delete(0, 'end')
+        self.pass_entry.delete(0, 'end')
+
     def signup(self):
         name = self.user_entry.get()
         passw = self.pass_entry.get()
         if name and passw:
             salt = Login.salt_generator()
             passw_hash = hashlib.sha256(str(passw+salt).encode())
+            print(passw_hash.hexdigest(), salt)
 
     def login(self):
         name = self.user_entry.get()
         passw = self.pass_entry.get()
         if name and passw:
+            try:
+                self.app.out_queue.put("request|salt|{}".format(name))
+                salt = self.app.in_queue.get(True, 4)
+                hash = hashlib.sha256(str(passw+salt).encode()).hexdigest()
+                self.app.out_queue.put("login|{}|{}".format(name, hash))
+                valid = self.app.in_queue.get(True, 4).split("|")
+                if valid[0] == "true":
+                    self.app.id = valid[1]
+                    self.app.username = name
+                    # do stuff
+                    self.undraw()
+                    self.app.main.draw()
+                elif valid[0] == "false":
+                    App.popup("info", "Invalid login credentials.")
+                    self.user_entry.delete(0, 'end')
+                    self.pass_entry.delete(0, 'end')
 
-            passw_hash = hashlib.sha256(passw+salt)
+            except queue.Empty as e:
+                App.popup("warning", "Could not establish a connection with server.")
 
+            except Exception as e:
+                print(e)
 
     @staticmethod
     def salt_generator(size=10):
         chars = string.ascii_uppercase + string.digits
         text = ''.join(random.choice(chars) for _ in range(size))
         return text
+
+
+class Main:
+    def __init__(self, app):
+        self.app = app
+
+        self.top_bar = tk.Frame(bg="royalblue4")
+
+        self.user_but = tk.Button(self.top_bar, text="Oi!", bg="royalblue3", fg="white",
+                                  font=("trebuchet ms", 12), padx=5, bd=0, command=self.load)
+
+        self.post_but = tk.Button(self.top_bar, text="Post Something", bg="royalblue3", fg="white", bd=0, width=14,
+                                  command=self.post, font=("trebuchet ms", 12))
+
+        self.logout_but = tk.Button(self.top_bar, text="Log Out", bg="royalblue3", fg="white", bd=0, width=7,
+                                    command=self.logout, font=("trebuchet ms", 12))
+
+        self.page_frame = tk.Frame(bg="gray90")
+
+        self.posts = []
+
+    def post(self):
+        t = PostDialog(self)
+
+    def load(self, id=-1):
+        self.clear_posts()
+        if id == -1:
+            self.app.out_queue.put("feed|{}".format(self.app.id))
+            feed = self.app.in_queue.get(True, 2)
+            feed = eval(feed)
+            for i, p in enumerate(feed):
+                u = p[0]
+                a = p[1]
+                m = p[2]
+                d = p[3]
+                t = p[4]
+                id = p[5]
+                act = a
+                if a == "run":
+                    act = "Ran {} kilometres.".format(m)
+                elif a == "swim":
+                    act = "Swam {} metres.".format(m)
+                self.posts.append(Post(container=self, username=u, activity=act, date=d, text=t, user_id=id))
+                self.posts[-1].draw(i)
+        else:
+            # query server for profile
+            self.app.out_queue.put("profile|{}|{}".format(id, self.app.id))
+            prof = self.app.in_queue.get(True, 2)
+            prof = eval(prof)
+            for i, p in enumerate(prof):
+                u = p[0]
+                a = p[1]
+                m = p[2]
+                d = p[3]
+                t = p[4]
+                act = a
+                if a == "run":
+                    act = "Ran {} kilometres.".format(m)
+                elif a == "swim":
+                    act = "Swam {} metres.".format(m)
+                self.posts.append(Post(container=self, username=u, activity=act, date=d, text=t, user_id=-1))
+                self.posts[-1].draw(i)
+        if len(self.posts) == 0:
+            self.posts.append(tk.Label(text="No posts here!", fg="gray65", bg="gray95",
+                                       font=("trebuchet ms", 12, "bold"), bd=2, relief="groove", padx=40, pady=20))
+            self.posts[0].grid(row=1, pady=10)
+
+    def logout(self):
+        self.app.id = -1
+        self.app.username = ""
+
+        self.undraw()
+        self.app.login_screen.draw()
+
+    def draw(self):
+        self.app.root.configure(bg="gray90")
+        self.top_bar.grid(column=0, row=0, sticky="NEWS")
+
+        self.user_but.grid(column=0, row=0, sticky="NSW", padx=(0, 120))
+        self.user_but.configure(text=self.app.username)
+        self.post_but.grid(column=50, row=0, sticky="NS", padx=(60, 60))
+        self.logout_but.grid(column=100, row=0, sticky="NSE", padx=(120, 0))
+
+        self.page_frame.grid(column=0, row=1)
+        self.load()
+
+    def undraw(self):
+        self.top_bar.grid_forget()
+        self.clear_posts()
+        self.page_frame.grid_forget()
+
+    def clear_posts(self):
+        for post in self.posts:
+            if isinstance(post, Post):
+                post.delete()
+            else:
+                post.destroy()
+        self.posts = []
 
 main = App()
--- gui-server.py
+++ gui-server.py
@@ -1,3 +1,4 @@
+import hashlib
 import socket
 import threading
 import queue
@@ -49,44 +50,100 @@ def handler(c, a):
                 print('received', reply)
                 split = reply.split("|")
                 # load|who|username|activities|timespan
+                msg = ''
                 if split[0] == "profile" and len(split) >= 3:
                     friend_id = split[1]
                     user_id = split[2]
                     if friend_id != user_id:
-                        db_in.put(
-                            """SELECT * FROM friends
-                               WHERE (friends.friend_id = {0} AND friends.id = {1})
-                               OR (friends.friend_id = {1} AND friends.id = {0})""".format(user_id, friend_id))
+                        query = """SELECT * FROM friends
+                                   WHERE (friends.friend_id = {0} AND friends.id = {1})
+                                   OR (friends.friend_id = {1} AND friends.id = {0})""".format(user_id, friend_id)
+                        db_in.put(query)
                         result = db_out.get(True, 2)
                     else:
                         result = True
                     if result:
-                        timespan = 4
+                        time1 = 'now'
+                        time2 = 7
                         activities = ()
                         flag = ""
-                        if len(split) == 5:
-                            timespan = split[4]
+                        if len(split) >= 5:
+                            time2 = split[4]
+                        if len(split) == 6:
+                            time1 = str(split[5]) + " day"
                         if len(split) >= 4:
                             activities = split[3]
                         if not activities:
                             flag = "NOT"
-                        query = ("""SELECT login.username, feed.activity, feed.metadata, feed.date
+                        query = ("""SELECT login.username, feed.activity, feed.metadata, feed.date, feed.text
                                     FROM login
                                     JOIN feed
                                     ON login.id=feed.id
-                                    WHERE date(feed.date) >= date('now', '-{} day')
+                                    WHERE date(feed.date) >= date('{}', '-{} day')
                                     AND feed.activity {} IN {}
                                     AND login.id={}
-                                    ORDER BY feed.date DESC""".format(timespan, flag, str(activities), friend_id))
+                                    ORDER BY feed.date DESC""".format(time1, time2, flag, str(activities), friend_id))
                         db_in.put(query)
                         result = db_out.get(True, 2)
                         if result:
-                            c.send('|'.join([str(x) for x in result]).encode())
+                            msg = str(result)
                     else:
                         print("not friends youth")
                 if split[0] == "feed" and len(split) >= 2:
                     user_id = split[1]
-                    print(user_id)
+                    time1 = 'now'
+                    time2 = 7
+                    if len(split) >= 3:
+                        time2 = split[2]
+                    if len(split) == 4:
+                        time1 = str(split[3]) + " day"
+                    query = """SELECT login.username, feed.activity, feed.metadata, feed.date, feed.text, login.id
+                               FROM login
+                               JOIN feed
+                               ON login.id=feed.id
+                               JOIN friends
+                               ON feed.id=friends.friend_id OR feed.id = friends.id
+                               WHERE date(feed.date) >= date('{}', '-{} day')
+                               AND friends.id = {}
+                               ORDER BY feed.date DESC""".format(time1, time2, user_id)
+                    db_in.put(query)
+                    result = db_out.get(True, 2)
+                    if result:
+                        msg = str(result)
+                if split[0] == "request" and len(split) == 3:
+                    what = split[1]
+                    if what == "salt":
+                        name = split[2]
+                        query = ("""SELECT login.salt, login.pass
+                                    FROM login
+                                    WHERE login.username='{}'""".format(name))
+                        db_in.put(query)
+                        result = db_out.get(True, 2)
+                        if result:
+                            salt = result[0][0]
+                            msg = salt
+                        else:
+                            msg = "impossible salt"
+                if split[0] == "login" and len(split) == 3:
+                    name = split[1]
+                    sub_passw = split[2]
+                    query = ("""SELECT login.pass, login.id
+                                FROM login
+                                WHERE login.username='{}'""".format(name))
+                    db_in.put(query)
+                    result = db_out.get(True, 2)
+                    if result:
+                        pass_hash = result[0][0]
+                        if pass_hash == sub_passw:
+                            msg = "true|" + str(result[0][1])
+                        else:
+                            msg = "false"
+                    else:
+                        msg = "false"
+
+                if msg:
+                    print("sending:", msg)
+                    c.send(msg.encode())
 
         except Exception as e:
             print(e)
